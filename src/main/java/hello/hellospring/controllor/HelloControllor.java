a3d81a09d283ef3162fd37b39f5a41d219d00c1e
은진 <80873499+eunjin2118@users.noreply.github.com>
2023-01-31 15:37:39 +0900

    MVC와 템플릿 엔진

--- src/main/java/hello/hellospring/controllor/HelloControllor.java
+++ src/main/java/hello/hellospring/controllor/HelloControllor.java
@@ -3,12 +3,22 @@ package hello.hellospring.controllor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloControllor {
     @GetMapping("hello")
     public String hello(Model model){
+        // 직접 받기
         model.addAttribute("data", "hello!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(
+            // 외부에서 url parameter로 넘겨온 name받기
+            @RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
