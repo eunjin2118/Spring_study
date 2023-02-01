2c92b0defe28c4136578962de8c1f8e47f0bef4f
은진 <80873499+eunjin2118@users.noreply.github.com>
2023-02-01 18:02:27 +0900

    API

--- src/main/java/hello/hellospring/controllor/HelloControllor.java
+++ src/main/java/hello/hellospring/controllor/HelloControllor.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloControllor {
@@ -20,5 +21,33 @@ public class HelloControllor {
             @RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // http에서 body부분에 내가 직접 내용을 넣어준다
+    public String helloString(@RequestParam("name") String name){
+        return "hello "+ name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    // JSON 방식
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        // 객체가 오면 Default가 객체를 JSON방식으로 저장하는 것
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
